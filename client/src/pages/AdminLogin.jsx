6756afab8b937c68ff8455367e29e971692c6b01
saket-bhadada <saketbhadada@gmail.com>
2026-07-25 21:49:07 +0530

    Fix missing Link import in AdminLogin

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { api } from "../lib/api.js";
 
 export default function AdminLogin() {
